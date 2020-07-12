8215f949d1553a2e615b47342d33edf439e40215
ryan <rynmcevoy21@gmail.com>
2020-07-12 19:59:22 -0400

    Initialize Commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var cont = " ";
+
+                do
+                {
+
+                    Console.WriteLine("Enter Length:");
+                    double length = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Width:");
+                    double width = double.Parse(Console.ReadLine());
+
+                    double area = length * width;
+                    double perimeter = 2 * (length + width);
+
+                    Console.WriteLine("Area: {0}", area);
+                    Console.WriteLine("Perimeter: {0}", perimeter);
+
+                    Console.WriteLine("Do you want to calculate another room? (Y/N)");
+                    cont = Console.ReadLine().ToUpper();
+                }
+                while (cont == "Y");
+            }
+        }
+    }
+}
+            
